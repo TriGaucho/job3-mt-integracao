2078103e169668682914aebf37c304bd51dfb0ac
Diogo Diarone Alves de Almeida <da50084@saqueepague.com.br>
2023-06-21 12:55:33 -0300

    removido modulo vuupt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import LoggerMiddleware from '@shared/middlewares/logger.middlewares'
 
 import configuracoes from '@modules/configuracao/routes/configuracao.routes'
-import vuupt from '@modules/vuupt/routes/vuupt.routes'
+// import vuupt from '@modules/vuupt/routes/vuupt.routes'
 
 const routes = Router()
 
 routes.use(LoggerMiddleware)
 
 routes.use('/config', configuracoes)
-routes.use('/vuupt', vuupt)
+// routes.use('/vuupt', vuupt)
 
 export default routes
